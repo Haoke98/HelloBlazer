81ddb6fb80a2b0ea3e26f7dd7a3b0c3af7675fef
Sadam·Sadik <1903249375@qq.com>
2024-01-07 17:45:02 +0800

    初始化项目

---
+++ HelloBlazer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HelloBlazer;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
